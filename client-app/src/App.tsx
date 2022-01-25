b64243df9be7bec8e969897ab9b85211022f27a0
elefint <31921189+elefint@users.noreply.github.com>
2022-01-25 03:15:29 +0000

    Section 3 - complete

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import {Header, List} from "semantic-ui-react";
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // useState activities is a variable for storing state
+    // useState setActivities is a function for setting the state
+    // initialState [] = empty array
+    const [activities, setActivities] = useState([]);
+
+    // useEffect function (arrow function with empty opening brackets for no parameters)
+    useEffect(() => {
+        // get json and then
+        axios.get('http://localhost:5000/api/activities').then(response => {
+            console.log(response);
+            // pass response to setActivities, setActivities passes the data from response to activities variable
+            setActivities(response.data);
+        })
+        // , after curly bracket and then empty array to ensure this runs once
+    }, [])
+
+
+    return (
+        <div>
+            <Header as="h2" icon="users" content="Reactivities"/>
+            <List>
+                {activities.map((activity: any) => (
+                    <List.Item key={activity.id}>
+                        {activity.title}
+                    </List.Item>
+                ))}
+            </List>
+        </div>
+    );
 }
 
 export default App;
